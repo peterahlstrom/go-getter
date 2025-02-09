c59fc29a53347412084466bccf949cc68565ed62
peterahlstrom <ahlstrom.peter@gmail.com>
2025-02-09 16:52:48 +0100

    feat: add auth

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Endpoint struct {
+	UrlPath 		string `json:"urlPath"`
+	ScriptPath 		string `json:"scriptPath"`
+	RequireAuth		bool `json:"requireAuth"`
+	ValidApiKeys	map[string]string `json:"apiKeys"`
+}
+
+type Config struct {
+	LogPath					string `json:"logPath"`
+	ConcurrentScriptsLimit 	int `json:"concurrentScriptsLimit"`
+	Endpoints				map[string]Endpoint `json:"endpoints"`
+}
+
+
+func GetConfig (configPath string) (*Config, error) {
+	var cfg Config
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config file: %v", err)
+	}
+
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse config-file: %v", err)
+	}
+	
+	return &cfg, nil
+} 
